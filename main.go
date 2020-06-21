99a70a4ee00c7245fe7724c7bbda63b6431521dd
Fernando Chahua Lopez <fernandochahua1@gmail.com>
2020-06-21 05:12:08 -0500

    adding rest

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
 )
 
 const (
@@ -33,39 +37,39 @@ const (
 //Gender_Female
 //Gender_Male
 //Contact_Dont-Know
-//Contact_No
-//Contact_Yes
+//Contact_no
+//Contact_yes
 //Results
 
-type persona struct {
-	fever                   float64
-	tiredness               float64
-	dry_cough               float64
-	difficulty_in_breathing float64
-	sore_throat             float64
-	no_sintomas             float64
-	pains                   float64
-	nasal_congestion        float64
-	runny_nose              float64
-	diarrhea                float64
-	no_other_sintomas       float64
-	edad_0_9                float64
-	edad_10_19              float64
-	edad_20_24              float64
-	edad_25_59              float64
-	edad_60_more            float64
-	female                  float64
-	male                    float64
-	contact_dk              float64
-	contact_no              float64
-	contact_yes             float64
-	results                 float64
+type Persona struct {
+	Fever                   float64 `json:"fever"`
+	Tiredness               float64 `json:"tiredness"`
+	Dry_cough               float64 `json:"dry_cough"`
+	Difficulty_in_breathing float64 `json:"difficulty_in_breathing"`
+	Sore_throat             float64 `json:"sore_throat"`
+	No_sintomas             float64 `json:"no_sintomas"`
+	Pains                   float64 `json:"pains"`
+	Nasal_congestion        float64 `json:"nasal_congestion"`
+	Runny_nose              float64 `json:"runny_nose"`
+	Diarrhea                float64 `json:"diarrhea"`
+	No_other_sintomas       float64 `json:"no_other_sintomas"`
+	Edad_0_9                float64 `json:"edad_0_9"`
+	Edad_10_19              float64 `json:"edad_10_19"`
+	Edad_20_24              float64 `json:"edad_20_24"`
+	Edad_25_59              float64 `json:"edad_25_59"`
+	Edad_60_more            float64 `json:"edad_60_more"`
+	Female                  float64 `json:"female"`
+	Male                    float64 `json:"male"`
+	Contact_dk              float64 `json:"contact_dk"`
+	Contact_no              float64 `json:"contact_no"`
+	Contact_yes             float64 `json:"contact_yes"`
+	Results                 float64 `json:"results"`
 }
 
-var listPersonas [cols]persona
+var listPersonas [cols]Persona
 var distancias [cols]float64
-var vecinos [K]persona
-var prueba persona
+var vecinos [K]Persona
+var prueba Persona
 
 func FloatToString(input_num float64) string {
 	// para convertir float a string
@@ -109,74 +113,78 @@ func leer_dataset() {
 		contacto_si, _ := strconv.ParseFloat(line[20], 64)
 		resultados, _ := strconv.ParseFloat(line[21], 64)
 
-		per := persona{
-			fever:                   fiebre,
-			tiredness:               cansancio,
-			dry_cough:               tos_seca,
-			difficulty_in_breathing: dificultad_respiratoria,
-			sore_throat:             dolor_garganta,
-			no_sintomas:             sin_sintomas,
-			pains:                   dolores,
-			nasal_congestion:        congestion_nasal,
-			runny_nose:              nariz_moqueo,
-			diarrhea:                diarrea,
-			no_other_sintomas:       sin_otros_sintomas,
-			edad_0_9:                edad_0a9,
-			edad_10_19:              edad_10a19,
-			edad_20_24:              edad_20a24,
-			edad_25_59:              edad_25a59,
-			edad_60_more:            edad_60aMas,
-			female:                  mujer,
-			male:                    hombre,
-			contact_dk:              contacto_nosabe,
-			contact_no:              contacto_no,
-			contact_yes:             contacto_si,
-			results:                 resultados,
+		per := Persona{
+			Fever:                   fiebre,
+			Tiredness:               cansancio,
+			Dry_cough:               tos_seca,
+			Difficulty_in_breathing: dificultad_respiratoria,
+			Sore_throat:             dolor_garganta,
+			No_sintomas:             sin_sintomas,
+			Pains:                   dolores,
+			Nasal_congestion:        congestion_nasal,
+			Runny_nose:              nariz_moqueo,
+			Diarrhea:                diarrea,
+			No_other_sintomas:       sin_otros_sintomas,
+			Edad_0_9:                edad_0a9,
+			Edad_10_19:              edad_10a19,
+			Edad_20_24:              edad_20a24,
+			Edad_25_59:              edad_25a59,
+			Edad_60_more:            edad_60aMas,
+			Female:                  mujer,
+			Male:                    hombre,
+			Contact_dk:              contacto_nosabe,
+			Contact_no:              contacto_no,
+			Contact_yes:             contacto_si,
+			Results:                 resultados,
 		}
 		listPersonas[i] = per
-		fmt.Println(FloatToString(per.fever) + " " + FloatToString(per.tiredness) +
-			" " + FloatToString(per.dry_cough) + " " + FloatToString(per.difficulty_in_breathing) +
-			" " + FloatToString(per.sore_throat) + " " + FloatToString(per.no_sintomas) +
-			" " + FloatToString(per.pains) + " " + FloatToString(per.nasal_congestion) +
-			" " + FloatToString(per.runny_nose) + " " + FloatToString(per.diarrhea) +
-			" " + FloatToString(per.no_other_sintomas) + " " + FloatToString(per.edad_0_9) +
-			" " + FloatToString(per.edad_10_19) + " " + FloatToString(per.edad_20_24) +
-			" " + FloatToString(per.edad_25_59) + " " + FloatToString(per.edad_60_more) +
-			" " + FloatToString(per.female) + " " + FloatToString(per.male) +
-			" " + FloatToString(per.contact_dk) + " " + FloatToString(per.contact_no) +
-			" " + FloatToString(per.contact_yes) + " " + FloatToString(per.results))
+		fmt.Println(FloatToString(per.Fever) + " " + FloatToString(per.Tiredness) +
+			" " + FloatToString(per.Dry_cough) + " " + FloatToString(per.Difficulty_in_breathing) +
+			" " + FloatToString(per.Sore_throat) + " " + FloatToString(per.No_sintomas) +
+			" " + FloatToString(per.Pains) + " " + FloatToString(per.Nasal_congestion) +
+			" " + FloatToString(per.Runny_nose) + " " + FloatToString(per.Diarrhea) +
+			" " + FloatToString(per.No_other_sintomas) + " " + FloatToString(per.Edad_0_9) +
+			" " + FloatToString(per.Edad_10_19) + " " + FloatToString(per.Edad_20_24) +
+			" " + FloatToString(per.Edad_25_59) + " " + FloatToString(per.Edad_60_more) +
+			" " + FloatToString(per.Female) + " " + FloatToString(per.Male) +
+			" " + FloatToString(per.Contact_dk) + " " + FloatToString(per.Contact_no) +
+			" " + FloatToString(per.Contact_yes) + " " + FloatToString(per.Results))
 	}
 }
 
-func dist_eucl(per1 persona, per2 persona, i int) {
+func dist_eucl(per1 Persona, per2 Persona, i int) {
 	distancia := 0.0
-	distancia = math.Pow(per1.fever-per2.fever, 2) +
-		math.Pow(per1.tiredness-per2.tiredness, 2) +
-		math.Pow(per1.dry_cough-per2.dry_cough, 2) +
-		math.Pow(per1.difficulty_in_breathing-per2.difficulty_in_breathing, 2) +
-		math.Pow(per1.sore_throat-per2.sore_throat, 2) +
-		math.Pow(per1.no_sintomas-per2.no_sintomas, 2) +
-		math.Pow(per1.pains-per2.pains, 2) +
-		math.Pow(per1.nasal_congestion-per2.nasal_congestion, 2) +
-		math.Pow(per1.runny_nose-per2.runny_nose, 2) +
-		math.Pow(per1.diarrhea-per2.diarrhea, 2) +
-		math.Pow(per1.no_other_sintomas-per2.no_other_sintomas, 2) +
-		math.Pow(per1.edad_0_9-per2.edad_0_9, 2) +
-		math.Pow(per1.edad_10_19-per2.edad_10_19, 2) +
-		math.Pow(per1.edad_20_24-per2.edad_20_24, 2) +
-		math.Pow(per1.edad_25_59-per2.edad_25_59, 2) +
-		math.Pow(per1.edad_60_more-per2.edad_60_more, 2) +
-		math.Pow(per1.female-per2.female, 2) +
-		math.Pow(per1.male-per2.male, 2) +
-		math.Pow(per1.contact_dk-per2.contact_dk, 2) +
-		math.Pow(per1.contact_no-per2.contact_no, 2) +
-		math.Pow(per1.contact_no-per2.contact_yes, 2)
+	distancia = math.Pow(per1.Fever-per2.Fever, 2) +
+		math.Pow(per1.Tiredness-per2.Tiredness, 2) +
+		math.Pow(per1.Dry_cough-per2.Dry_cough, 2) +
+		math.Pow(per1.Difficulty_in_breathing-per2.Difficulty_in_breathing, 2) +
+		math.Pow(per1.Sore_throat-per2.Sore_throat, 2) +
+		math.Pow(per1.No_sintomas-per2.No_sintomas, 2) +
+		math.Pow(per1.Pains-per2.Pains, 2) +
+		math.Pow(per1.Nasal_congestion-per2.Nasal_congestion, 2) +
+		math.Pow(per1.Runny_nose-per2.Runny_nose, 2) +
+		math.Pow(per1.Diarrhea-per2.Diarrhea, 2) +
+		math.Pow(per1.No_other_sintomas-per2.No_other_sintomas, 2) +
+		math.Pow(per1.Edad_0_9-per2.Edad_0_9, 2) +
+		math.Pow(per1.Edad_10_19-per2.Edad_10_19, 2) +
+		math.Pow(per1.Edad_20_24-per2.Edad_20_24, 2) +
+		math.Pow(per1.Edad_25_59-per2.Edad_25_59, 2) +
+		math.Pow(per1.Edad_60_more-per2.Edad_60_more, 2) +
+		math.Pow(per1.Female-per2.Female, 2) +
+		math.Pow(per1.Male-per2.Male, 2) +
+		math.Pow(per1.Contact_dk-per2.Contact_dk, 2) +
+		math.Pow(per1.Contact_no-per2.Contact_no, 2) +
+		math.Pow(per1.Contact_no-per2.Contact_yes, 2)
 
 	distancias[i] = distancia
 }
 
+type ResultData struct {
+	Result int `json:"result"`
+}
+
 //KNN : Encuetra los vecinos mas cercanos
-func KNN(prueba persona) {
+func KNN(prueba Persona) {
 	ch := make(chan int, len(listPersonas))
 	var wg sync.WaitGroup
 	wg.Add(len(listPersonas))
@@ -212,13 +220,30 @@ func KNN(prueba persona) {
 
 }
 
+func PredecirEndPoint(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var person Persona
+	_ = json.NewDecoder(request.Body).Decode(&person)
+
+	//----------------
+	//--algoritmo---
+	KNN(person)
+	var result = predecir()
+	var resultData ResultData
+	resultData.Result = result
+	//dato devuelto
+	json.NewEncoder(w).Encode(resultData)
+}
+
+
 //metodo que predice el tipo de cancer dependeiendo de los vecinos mas cercanos
 func predecir() int {
 	prediccion := 0
 	var contadorM int
 	var contadorB int
 	for _, vecino := range vecinos {
-		if vecino.results == 1 {
+		if vecino.Results == 1 {
 			contadorM++
 		} else {
 			contadorB++
@@ -232,9 +257,18 @@ func predecir() int {
 
 func main() {
 	leer_dataset()
-	prueba := persona{1.000000, 1.000000, 1.000000, 1.000000, 0.000000, 0.000000, 0.000000,
+	prueba := Persona{1.000000, 1.000000, 1.000000, 1.000000, 0.000000, 0.000000, 0.000000,
 		1.000000, 1.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
 		1.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 1.000000}
 	KNN(prueba)
 	fmt.Println(predecir())
+
+
+	router := mux.NewRouter()
+	// endpoints
+	router.HandleFunc("/KNN", PredecirEndPoint).Methods("POST", "OPTIONS")
+	http.ListenAndServe(":3000", router)
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+
 }
